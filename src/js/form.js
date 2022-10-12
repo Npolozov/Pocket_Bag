5da98df7910e90e3a66010e95015354b80095cf9
Nikita Polozov <npolozov0@gmail.com>
2022-10-12 16:25:13 +0300

    !

--- src/js/form.js
+++ src/js/form.js
@@ -1,5 +1,14 @@
+import { Notify } from "notiflix";
+
 
 const form = document.getElementById('form')
+const formInput = document.querySelectorAll('._req');
+console.log(formInput);
+const btnSubmit = document.querySelector('.btn');
+console.log(btnSubmit);
+const mailInput = document.querySelector('._email');
+console.log(mailInput);
+
 
 
 form.addEventListener('submit', formSend);
@@ -10,27 +19,53 @@ function formSend(e) {
     e.preventDefault();
 
     let self = e.currentTarget;
+    let error = 0;
+
+    formInput.forEach(input => {
+        const inputName = input.name;
+        console.log(inputName)
+        if(input.value === '') {  
+          Notify.failure(`Заполните поле ${inputName}` ,{
+                position: "center-center",
+                timeout: 1000
+            });
+            error ++;
+        }
+     })
+
+     if(emailTest(mailInput)) {
+        Notify.failure('Не коректна пошта' ,{
+            position: "center-center",
+            timeout: 1000
+        });
+        error ++;
+     }
+     console.log(error);
 
     // отправка
 		let formData = new FormData(self);
 		let xhr = new XMLHttpRequest();
 
-        console.log(formData);
-
-        formData.forEach(item => {
-            console.log(item);
-        })
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState === 4) {
-				if (xhr.status === 200) {
-					console.log('Отправлено');
+        if (error === 0) {
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        Notify.success('Отправлено' , {
+                            position: "center-center",
+                            timeout: 1000
+                        });
+                    }
                 }
             }
+    
+            xhr.open('POST', 'mail.php', true);
+            xhr.send(formData);
+            e.currentTarget.reset();
         }
+}
 
-        xhr.open('POST', 'mail.php', true);
-		xhr.send(formData);
-        e.currentTarget.reset();
+function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
 }
 
 
--- src/mail.php
+++ src/mail.php
@@ -4,26 +4,43 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/SMTP.php';
+
+
+$title = "Заказ";
 
 $admin_email = array();
 foreach ( $_POST["admin_email"] as $key => $value ) {
 	array_push($admin_email, $value);
 }
 
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8'; 
-$mail->IsHTML(true);
+$c = true;
+// Формирование самого письма
+$title = "Заголовок письма";
+foreach ( $_POST as $key => $value ) {
+  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+    $body .= "
+    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+    </tr>
+    ";
+  }
+}
 
-$name = $_POST['name'];
-$phone = $_POST['tel'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$body = "<table style='width: 100%;'>$body</table>";
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->isSMTP();
+
+
+
+
+$mail->Host = 'ssl://smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'npolozov0@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'mfeasxabceqdpfsm'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = 'pnbgmwzuigyvthaz'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; 
 
@@ -34,10 +51,12 @@ foreach ( $admin_email as $key => $value ) {
 	$mail->addAddress($value);
 }
 
-
-$mail->Subject = 'Заявка';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
-$mail->AltBody = '';
-
+$mail->IsHTML(true);
+// $mail->Subject = 'Заявка';
+// $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email. '<br>Сообщение этого пользователя: ' .$message;
+// $mail->AltBody = '';
+$mail->Subject = $title;
+$mail->Body = $body;
 $mail->send();
+
 ?>
