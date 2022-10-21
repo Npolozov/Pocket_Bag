238cf458fa691d3802dfa8ea5fb85a9d22078502
Npolozov <103377917+Npolozov@users.noreply.github.com>
2022-10-21 17:00:59 +0000

    Deploying to gh-pages from @ Npolozov/Pocket_bag@e6199e95e09ead052f5037d09dcd4075a494dedc 🚀

---
+++ scroll-top.d8cacf23.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("isQdP",(function(o,l){var e=document.querySelector(".scroll-to-top");console.log(e),window.onscroll=function(){window.scrollY>400?e.classList.remove("scroll-hide"):window.scrollY<400&&e.classList.add("scroll-hide"),e.onclick=function(){window.scrollTo(0,0)}}}));
+//# sourceMappingURL=scroll-top.d8cacf23.js.map
