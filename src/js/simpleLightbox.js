a8a46145b6482f8e2214c4517298070fe794a6b0
Nikita Polozov <npolozov0@gmail.com>
2022-11-22 11:37:54 +0200

    1

--- src/js/simpleLightbox.js
+++ src/js/simpleLightbox.js
@@ -1,6 +1,9 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const gallery = document.querySelector('.gallery_item a');
+console.log(gallery);
+
 let lightbox = new SimpleLightbox('.gallery_item a', {
   captions: true,
   // captionsData: 'alt',
